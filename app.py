53b5ea26473ea6b263f6a99950fc234fd9d44812
loganvaleski <logan@freeplay.ai>
2023-07-05 19:13:40 -0600

    Added basic calendar view

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import calendar
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    year = datetime.now().year
+    month = datetime.now().month
+    _, num_days = calendar.monthrange(year, month)
+    days = list(range(1, num_days + 1))
+    return render_template('home.html', days=days)
 
 if __name__ == '__main__':
     app.run(debug=True)
