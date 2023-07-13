6adf3d53dccc0c0d2d5dce076dfdf92703b583da
loganvaleski <logan@freeplay.ai>
2023-07-13 09:41:44 -0600

    Added Habit DB and initialized

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template
 import calendar
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///habits.db'  # Using SQLite for simplicity
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Habit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    habit = db.Column(db.String(64), index=True)
+    status = db.Column(db.String(64), index=True)
+    date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
 
 @app.route('/')
 def home():
